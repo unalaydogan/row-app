11cbefde23e6efdd2b6ff01bb3dafae69579f4d9
Ünal Aydoğan <uaydogan12@gmail.com>
2025-01-28 22:15:43 +0300

    simple row-app react code
    for now only navbar is added

---
+++ src/Navbar.jsx
@@ -0,0 +1,17 @@
+export default function Navbar() {
+    return (
+        <nav>
+            <div>
+                
+                <img id="nav-logo" src="./images/kurek.jpeg" alt="odtü kürek logosu" />
+                <span>ODTÜ KÜREK TAKIMI</span>
+            </div>
+            <ul>
+                    <li><a href="tarih.html">Tarihimiz</a></li>
+                    <li><a href="iletisim.html">İletişim</a></li>
+                    <li><a href="randevu.html">Randevu Al</a></li>
+            </ul>
+
+        </nav>
+    )
+}
